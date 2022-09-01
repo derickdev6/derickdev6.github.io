24d8a5e9c0002263dd007a9eb52c71866c460606
DerickMacbookAir <derickdev6@gmail.com>
2022-09-01 09:27:57 -0500

    Punto 3

---
+++ practiceGuides/solved/T1M2022-02/Punto3.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Punto3 {
+    public static void main(String[] args) {
+        try {
+            File f = new File("test.txt");
+            Scanner scan = new Scanner(f);
+
+            String estudianteMax = "";
+            float maxProm = 0;
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                String[] tokens = line.split(",");
+                String estudiante = tokens[0];
+                System.out.println(estudiante);
+                float acum = 0;
+                float prom = 0;
+                for (int i = 1; i < tokens.length; i++) {
+                    System.out.print(tokens[i] + "  ");
+                    float nota = Float.parseFloat(tokens[i]);
+                    acum += nota;
+                }
+                prom = acum / 3;
+                if (prom > maxProm) {
+                    maxProm = prom;
+                    estudianteMax = estudiante;
+                }
+                System.out.print("Promedio =" + prom + "\n");
+            }
+            System.out.println("Promedio mas alto de " + estudianteMax + " con promedio de " + maxProm);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+        }
+    }
+}
